098b275ca8deefd22ac6f32a41e95c86312e156d
Sean Terry <seanterry@outlook.com>
2020-09-08 17:56:30 -0400

    Adapter WIP

---
+++ src/Validation/IValidatable.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+namespace Shipwright.Validation
+{
+    /// <summary>
+    /// Marker interface that indicates a type requires a validator.
+    /// </summary>
+
+    public interface IValidatable { }
+}
---
+++ src/Validation/ValidationAdapter.cs
@@ -0,0 +1,67 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shipwright.Validation
+{
+    /// <summary>
+    /// Adapter for encapsulating validation functionality.
+    /// </summary>
+    /// <typeparam name="T">Type to validate.</typeparam>
+
+    public class ValidationAdapter<T> : IValidationAdapter<T>
+    {
+        private readonly IEnumerable<IValidator<T>> validators;
+
+        /// <summary>
+        /// Constructs and adapter for encapsulating validation functionality.
+        /// </summary>
+        /// <param name="validators">Collection of validators defiend for the type.</param>
+
+        public ValidationAdapter( IEnumerable<IValidator<T>> validators )
+        {
+            this.validators = validators ?? throw new ArgumentNullException( nameof( validators ) );
+        }
+
+        /// <summary>
+        /// Validates the given object instance.
+        /// </summary>
+        /// <param name="instance">Object instance to validate.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>The validation result.</returns>
+        /// <exception cref="ArgumentNullException"/>
+
+        public async Task<ValidationResult> Validate( T instance, CancellationToken cancellationToken )
+        {
+            if ( instance == null ) throw new ArgumentNullException( nameof( instance ) );
+
+            if ( instance is IValidatable && !validators.Any() )
+            {
+                throw new InvalidOperationException( string.Format( Resources.CoreErrorMessages.MissingRequiredValidator, typeof( T ) ) );
+            }
+
+            var errors = new List<ValidationFailure>();
+
+            foreach ( var validator in validators )
+            {
+                var result = validator.Validate( instance );
+
+                if ( !result.IsValid )
+                {
+                    errors.AddRange( result.Errors );
+                }
+            }
+
+            return new ValidationResult( errors );
+        }
+    }
+}
