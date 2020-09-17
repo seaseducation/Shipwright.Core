bb71cf0e172c0ffa828d4ee6f034b60a43316749
Sean Terry <seanterry@outlook.com>
2020-09-17 17:07:42 -0400

    Lamar registration

---
+++ lamar/src/Dataflows/LamarDataflowSourceExtensions.cs
@@ -0,0 +1,76 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+using Lamar;
+using Lamar.Scanning.Conventions;
+using Shipwright.Dataflows.Sources;
+using Shipwright.Dataflows.Sources.Internal;
+using System;
+
+namespace Shipwright.Dataflows
+{
+    /// <summary>
+    /// Extension methods for registering dataflow types with Lamar.
+    /// </summary>
+
+    public static class LamarDataflowSourceExtensions
+    {
+        /// <summary>
+        /// Adds the stock Shipwright dataflow record source dispatcher.
+        /// </summary>
+        /// <param name="registry">Lamar service registry.</param>
+        /// <returns>The service registry.</returns>
+
+        public static ServiceRegistry AddSourceDispatch( this ServiceRegistry registry )
+        {
+            if ( registry == null ) throw new ArgumentNullException( nameof( registry ) );
+
+            registry.For<ISourceDispatcher>().Add<SourceDispatcher>();
+            return registry;
+        }
+
+        /// <summary>
+        /// Adds all implementations of <see cref="ISourceHandler{TSource}"/> found in the scanned assemblies.
+        /// </summary>
+        /// <param name="scanner">Lamar assembly scanner.</param>
+        /// <returns>The assembly scanner.</returns>
+
+        public static IAssemblyScanner AddSourceHandlers( this IAssemblyScanner scanner )
+        {
+            if ( scanner == null ) throw new ArgumentNullException( nameof( scanner ) );
+
+            scanner.ConnectImplementationsToTypesClosing( typeof( ISourceHandler<> ) );
+            return scanner;
+        }
+
+        /// <summary>
+        /// Adds the validation decorator to all implementations of <see cref="ISourceHandler{TSource}"/>.
+        /// </summary>
+        /// <param name="registry">Lamar service registry.</param>
+        /// <returns>The service registry.</returns>
+
+        public static ServiceRegistry AddSourceValidation( this ServiceRegistry registry )
+        {
+            if ( registry == null ) throw new ArgumentNullException( nameof( registry ) );
+
+            registry.For( typeof( ISourceHandler<> ) ).DecorateAllWith( typeof( ValidationDecorator<> ) );
+            return registry;
+        }
+
+        /// <summary>
+        /// Adds the cancellation decorator to all implementations of <see cref="ISourceHandler{TSource}"/>.
+        /// </summary>
+        /// <param name="registry">Lamar service registry.</param>
+        /// <returns>The service registry.</returns>
+
+        public static ServiceRegistry AddSourceCancellation( this ServiceRegistry registry )
+        {
+            if ( registry == null ) throw new ArgumentNullException( nameof( registry ) );
+
+            registry.For( typeof( ISourceHandler<> ) ).DecorateAllWith( typeof( CancellationDecorator<> ) );
+            return registry;
+        }
+    }
+}
---
+++ lamar/test/Dataflows/LamarDataflowSourceExtensionsTests.cs
@@ -0,0 +1,139 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+using Lamar;
+using Lamar.Scanning.Conventions;
+using Moq;
+using Shipwright.Dataflows.Sources;
+using Shipwright.Dataflows.Sources.Internal;
+using Shipwright.Validation;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Xunit;
+
+namespace Shipwright.Dataflows
+{
+    public class LamarDataflowSourceExtensionsTests
+    {
+        private ServiceRegistry registry = new ServiceRegistry();
+
+        public class AddSourceDispatch : LamarDataflowSourceExtensionsTests
+        {
+            private ServiceRegistry method() => registry.AddSourceDispatch();
+
+            [Fact]
+            public void requires_registry()
+            {
+                registry = null!;
+                Assert.Throws<ArgumentNullException>( nameof( registry ), method );
+            }
+
+            [Fact]
+            public void registers_dispatcher()
+            {
+                var actual = method();
+                Assert.Same( registry, actual );
+
+                using var container = new Container( actual );
+                var instance = container.GetInstance<ISourceDispatcher>();
+                Assert.IsType<SourceDispatcher>( instance );
+            }
+        }
+
+        public class AddSourceHandlers : LamarDataflowSourceExtensionsTests
+        {
+            private IAssemblyScanner scanner;
+            private IAssemblyScanner method() => scanner.AddSourceHandlers();
+
+            public class FakeSourceHandler : SourceHandler<FakeSource>
+            {
+                protected override IAsyncEnumerable<Record> Read( FakeSource source, StringComparer comparer, CancellationToken cancellationToken ) => throw new NotImplementedException();
+            }
+
+            [Fact]
+            public void requires_scanner()
+            {
+                scanner = null!;
+                Assert.Throws<ArgumentNullException>( nameof( scanner ), method );
+            }
+
+            [Fact]
+            public void registers_discovered_handlers()
+            {
+                registry.Scan( scanner =>
+                {
+                    this.scanner = scanner;
+                    scanner.AssemblyContainingType<Source>(); // sanity check to ensure decorators aren't grabbed
+                    scanner.AssemblyContainingType<AddSourceHandlers>();
+
+                    var actual = method();
+                    Assert.Same( this.scanner, actual );
+                } );
+
+                using var container = new Container( registry );
+                var instance = container.GetInstance<ISourceHandler<FakeSource>>();
+                Assert.IsType<FakeSourceHandler>( instance );
+            }
+        }
+
+        public class AddSourceValidation : LamarDataflowSourceExtensionsTests
+        {
+            private ServiceRegistry method() => registry.AddSourceValidation();
+
+            [Fact]
+            public void requires_registry()
+            {
+                registry = null!;
+                Assert.Throws<ArgumentNullException>( nameof( registry ), method );
+            }
+
+            [Fact]
+            public void decorates_handler()
+            {
+                var inner = Mockery.Of<ISourceHandler<FakeSource>>();
+                var validator = Mockery.Of<IValidationAdapter<FakeSource>>();
+                registry.For<ISourceHandler<FakeSource>>().Add( inner );
+                registry.For<IValidationAdapter<FakeSource>>().Add( validator );
+
+                var actual = method();
+                Assert.Same( registry, actual );
+
+                using var container = new Container( registry );
+                var instance = container.GetInstance<ISourceHandler<FakeSource>>();
+                var decorator = Assert.IsType<ValidationDecorator<FakeSource>>( instance );
+                Assert.Same( inner, decorator.inner );
+                Assert.Same( validator, decorator.validator );
+            }
+        }
+
+        public class AddSourceCancellation : LamarDataflowSourceExtensionsTests
+        {
+            private ServiceRegistry method() => registry.AddSourceCancellation();
+
+            [Fact]
+            public void requires_registry()
+            {
+                registry = null!;
+                Assert.Throws<ArgumentNullException>( nameof( registry ), method );
+            }
+
+            [Fact]
+            public void decorates_handler()
+            {
+                var inner = Mockery.Of<ISourceHandler<FakeSource>>();
+                registry.For<ISourceHandler<FakeSource>>().Add( inner );
+
+                var actual = method();
+                Assert.Same( registry, actual );
+
+                using var container = new Container( registry );
+                var instance = container.GetInstance<ISourceHandler<FakeSource>>();
+                var decorator = Assert.IsType<CancellationDecorator<FakeSource>>( instance );
+                Assert.Same( inner, decorator.inner );
+            }
+        }
+    }
+}
