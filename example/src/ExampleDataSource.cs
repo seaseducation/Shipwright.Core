b3a794f9e8f6ee482db4a68d028b9f07b1258add
Sean Terry <seanterry@outlook.com>
2020-09-23 21:37:35 -0400

    Working example

---
+++ example/src/ExampleDataSource.cs
@@ -0,0 +1,47 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+using FluentValidation;
+using Shipwright.Dataflows;
+using Shipwright.Dataflows.Sources;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Shipwright.Core.Example
+{
+    public record ExampleDataSource : Source
+    {
+        public int Records { get; init; }
+
+        public class Validator : AbstractValidator<ExampleDataSource>
+        {
+            public Validator()
+            {
+                RuleFor( _ => _.Records ).GreaterThan( 0 );
+            }
+        }
+
+        public class Handler : ISourceHandler<ExampleDataSource>
+        {
+            public async IAsyncEnumerable<Record> Read( ExampleDataSource source, Dataflow dataflow, [EnumeratorCancellation] CancellationToken cancellationToken )
+            {
+                if ( source == null ) throw new ArgumentNullException( nameof( source ) );
+                if ( dataflow == null ) throw new ArgumentNullException( nameof( dataflow ) );
+
+                for ( var i = 0; i < source.Records; i++ )
+                {
+                    var data = new Dictionary<string, object>
+                    {
+                        { "value", i },
+                    };
+
+                    yield return new Record( dataflow, source, data, i );
+                }
+            }
+        }
+    }
+}
---
+++ example/src/Program.cs
@@ -0,0 +1,77 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+using Lamar.Microsoft.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shipwright.Commands;
+using Shipwright.Dataflows;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shipwright.Core.Example
+{
+    public class Program : BackgroundService
+    {
+        private static async Task Main( string[] args )
+        {
+            await CreateHostBuilder( args )
+                .RunConsoleAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder( string[] args ) =>
+            Host.CreateDefaultBuilder( args )
+                .UseLamar( ( context, registry ) =>
+                {
+                    registry
+                        .AddValidationAdapter()
+                        .AddCommandDispatch()
+                        .AddCommandValidation()
+                        .AddCommandCancellation()
+                        .AddDataflow();
+
+                    registry.Scan( scanner =>
+                    {
+                        scanner.AssemblyContainingType<Dataflow>();
+                        scanner.AssemblyContainingType<Program>();
+
+                        scanner
+                            .AddValidators()
+                            .AddCommandHandlers()
+                            .AddDataflowImplementations();
+                    } );
+
+                    registry.AddHostedService<Program>();
+                } );
+
+        private readonly ICommandDispatcher dispatcher;
+
+        public Program( ICommandDispatcher dispatcher )
+        {
+            this.dispatcher = dispatcher ?? throw new ArgumentNullException( nameof( dispatcher ) );
+        }
+
+        protected override async Task ExecuteAsync( CancellationToken stoppingToken )
+        {
+            var dataflow = new Dataflow
+            {
+                Name = "FizzBuzz",
+
+                Sources =
+                {
+                    new ExampleDataSource { Records = 100 },
+                },
+
+                Transformations =
+                {
+                    new ExampleTransformation { Fizz = 3, Buzz = 5 },
+                },
+            };
+
+            await dispatcher.Execute( dataflow, stoppingToken );
+        }
+    }
+}
