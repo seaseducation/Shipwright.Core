c3869a58cf729c10d9b2ef4cf6164eb0d8f5ea37
Sean Terry <seanterry@outlook.com>
2020-10-14 21:39:07 -0400

    Database lookup transformation WIP

---
+++ src/Dataflows/Transformations/DbLookup.Factory.cs
@@ -0,0 +1,45 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+using Shipwright.Databases;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shipwright.Dataflows.Transformations
+{
+    public partial record DbLookup
+    {
+        /// <summary>
+        /// Factory for the <see cref="DbLookup"/> transformation.
+        /// </summary>
+
+        public class Factory : ITransformationFactory<DbLookup>
+        {
+            private readonly IDbConnectionDispatcher dispatcher;
+
+            /// <summary>
+            /// Factory for the <see cref="DbLookup"/> transformation.
+            /// </summary>
+
+            public Factory( IDbConnectionDispatcher dispatcher )
+            {
+                this.dispatcher = dispatcher ?? throw new ArgumentNullException( nameof( dispatcher ) );
+            }
+
+            /// <summary>
+            /// Creates a handler for the transformation.
+            /// </summary>
+
+            public async Task<ITransformationHandler> Create( DbLookup transformation, CancellationToken cancellationToken )
+            {
+                if ( transformation == null ) throw new ArgumentNullException( nameof( transformation ) );
+
+                var connectionFactory = await dispatcher.Build( transformation.ConnectionInfo, cancellationToken );
+                return new Handler( transformation, connectionFactory );
+            }
+        }
+    }
+}
---
+++ src/Dataflows/Transformations/DbLookup.FailureEventSetting.cs
@@ -0,0 +1,40 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 TTCO Holding Company, Inc. and Contributors
+// Licensed under the Apache License, Version 2.0
+// See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
+
+using Microsoft.Extensions.Logging;
+
+namespace Shipwright.Dataflows.Transformations
+{
+    public partial record DbLookup
+    {
+        /// <summary>
+        /// Describes settings to use for lookup failure events.
+        /// </summary>
+
+        public record FailureEventSetting
+        {
+            /// <summary>
+            /// Whether the lookup failure should stop record processing.
+            /// Defaults to true.
+            /// </summary>
+
+            public bool IsFatal { get; set; } = true;
+
+            /// <summary>
+            /// Log level to use for lookup failure events.
+            /// Defaults to <see cref="LogLevel.Error"/>.
+            /// </summary>
+
+            public LogLevel Level { get; set; } = LogLevel.Error;
+
+            /// <summary>
+            /// Delegate for generating failure event messages.
+            /// </summary>
+
+            public FailureEventMessageDelegate FailureEventMessage { get; init; } = count =>
+                string.Format( Resources.CoreErrorMessages.DbLookupFailureMessage, count );
+        }
+    }
+}
