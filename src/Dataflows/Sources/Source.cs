e08c794de8aec8b756de90d33d0557618db7ef15
Sean Terry <seanterry@outlook.com>
2020-12-03 16:18:01 -0500

    Added optional description to data sources

--- src/Dataflows/Sources/Source.cs
+++ src/Dataflows/Sources/Source.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Copyright 2020 TTCO Holding Company, Inc. and Contributors
 // Licensed under the Apache License, Version 2.0
 // See https://opensource.org/licenses/Apache-2.0 or the LICENSE file in the repository root for the full text of the license.
@@ -11,5 +11,13 @@ namespace Shipwright.Dataflows.Sources
     /// Defines a dataflow record source.
     /// </summary>
 
-    public abstract record Source : IRequiresValidation { }
+    public abstract record Source : IRequiresValidation
+    {
+        /// <summary>
+        /// Description to use for the data source.
+        /// This value can be used for logging.
+        /// </summary>
+
+        public string? Description { get; init; }
+    }
 }
